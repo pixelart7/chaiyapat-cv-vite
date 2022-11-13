a4c55c56726b8a627d98c01a7606f9ac4f17fe56
Chaiyapat Tantiworachot <chaiyapat@pix7.me>
2022-11-13 21:36:23 +0900

    feat: tweak UX of nav on mobile

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,16 +1,19 @@
 <template lang="pug">
-.navigation.px-3.py-1.leading-relaxed.border-b(
+.navigation.leading-relaxed.border-b(
   ref="navigation"
   class="sm:border-b sm:border-l sm:border-r border-black/10 bg-white/40 backdrop-blur-lg sm:rounded-br-xl sm:rounded-bl-xl"
 )
-  .flex.flex-wrap.items-center.-mx-1
-    .inline.mr-1.text-sm.text-primary-900.w-full.px-1.mb-2.opacity-70.mt-2(class="sm:w-auto sm:mb-0 sm:mt-0") Jump to:
-    a.px-3.py-1.m-1.rounded-full.text-sm.border.font-medium.text-gray-700(
-      v-for="(name, link) in links"
-      :href="`#${link}`"
-      @click.prevent="smoothTransition(link)"
-      class="border-gray-800/10 bg-white/70 hover:bg-white/90 hover:border-primary-700"
-    ) {{name}}
+  .overflow-x-auto.px-3.py-1(class="sm:overflow-x-visible")
+    .flex.flex-nowrap.items-center.-mx-1(class="sm:flex-wrap")
+      .inline.mr-1.text-sm.text-primary-900.w-full.px-1.mb-2.opacity-70.mt-2.whitespace-nowrap(class="sm:w-auto sm:mb-0 sm:mt-0 sm:whitespace-normal") Jump to:
+      a.px-3.py-1.m-1.rounded-full.text-sm.border.font-medium.text-gray-700.whitespace-nowrap(
+        v-for="(name, link) in links"
+        :id="`nav-${link}`"
+        :href="`#${link}`"
+        @click.prevent="smoothTransition(link)"
+        class="border-gray-800/10 bg-white/70 hover:bg-white/90 hover:border-primary-700 sm:whitespace-normal"
+      ) {{name}}
+      .w-6(class="sm:hidden") &nbsp;
 </template>
 
 <script lang="ts">
@@ -19,6 +22,17 @@ import { defineComponent, ref } from 'vue';
 export default defineComponent({
   setup() {
     const navigation = ref(null);
+
+    const links = {
+      top: 'Intro',
+      about: 'About',
+      work: 'Work Experiences',
+      education: 'Education',
+      skill: 'Skill',
+      language: 'Languages',
+      'side-project': 'Side Projects',
+      interest: 'Interests',
+    }
 
     function smoothTransition(elementId: string) {
       const section = document.getElementById(elementId);
@@ -38,16 +52,7 @@ export default defineComponent({
     return {
       navigation,
       smoothTransition,
-      links: {
-        top: 'Intro',
-        about: 'About',
-        work: 'Work Experiences',
-        education: 'Education',
-        skill: 'Skill',
-        language: 'Languages',
-        'side-project': 'Side Projects',
-        interest: 'Interests',
-      }
+      links,
     }
   },
 });
