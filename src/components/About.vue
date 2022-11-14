971b9915df45cc87a090cc84e0807483d79da8b9
Chaiyapat Tantiworachot <chaiyapat@pix7.me>
2022-11-14 10:41:29 +0900

    fix: about

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
 #about.bg-white.rounded-xl.p-6(class="print:p-0")
   h2.text-2xl.font-bold.mb-3.mt-2 👋 Hello!
-  .text-sm.space-y-3
+  .space-y-3
     p Welcome! I want to use this little section to tell you about myself 🤓
-    p I started my professional career back in high school. I hosted a Minecraft, I built a website for that server, and in turn, I learned web development. After that, I fell in love with front-end development. I cherish the process of developing and seeing how people interact with the front-end part of the system.
+    p I started my professional career back in high school. I hosted a Minecraft server, I built a website for that server, and in turn, I learned web development. After that, I fell in love with front-end development. I cherish the process of developing and seeing how people interact with the front-end part of the system.
     p In college, I spent quite some time working as a freelance. I landed in a company where I proudly call myself a solution engineer. I'm confident that I can provide a solution, or, at the very least, I know who to ask.
     p Curiously, I want to learn more about how to conduct research properly. In 2021, I received a scholarship to study aboard for a Master's degree in Japan. 
     p <span class="font-medium text-primary-800">Currently,</span> I'm in a lab that studies physically-augmented interaction. I am interested to answer "how can we take a break from this busy world, taking information slowly."
