22cca405184fc911a30d2c1d59853d8f4d0ea5ec
Chaiyapat Tantiworachot <chaiyapat@pix7.me>
2023-01-06 17:04:38 +0900

    feat: make button more clickable

--- src/components/MovingForward.vue
+++ src/components/MovingForward.vue
@@ -13,8 +13,10 @@
           Thank you for visiting! I'm moving my site from using GitHub Pages (pixelart7.github.io) to my domain (<a href="https://chaiyapat.pix7.me" class="hover:underline hover:opacity-75">chaiyapat.pix7.me</a>).
           The new site is redesigned and reimagined from the ground up. There's also a new digital garden section for me to post some stuff.
           I will set an automatic redirection when I finished tidying up! See you there :)
-        .text-right.mt-3
-          a.bg-primary-500.px-3.py-2.rounded.text-black.inline-block.tracking-wider(class="hover:bg-primary-400" href="https://chaiyapat.pix7.me") To the new site
+        .text-right.mt-3(class="sm:mt-6")
+          a.bg-primary-500.px-3.py-2.rounded-lg.text-black.inline-block.tracking-wider.border.border-primary-900(
+            class="hover:bg-primary-400" href="https://chaiyapat.pix7.me"
+          ) To the new site
 </template>
 
 <script lang="ts">
